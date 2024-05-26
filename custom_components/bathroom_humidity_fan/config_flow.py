3aeca988b9c4dffcb1de8d42007368ebaac989c5
freedomanonyme <58370978+freedomanonyme@users.noreply.github.com>
2024-05-26 11:48:52 -0400

    Update config_flow.py

--- custom_components/bathroom_humidity_fan/config_flow.py
+++ custom_components/bathroom_humidity_fan/config_flow.py
@@ -1,50 +1,35 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers.selector import EntitySelector
+from homeassistant.helpers import config_validation as cv
 
-from .const import DOMAIN, CONF_SENSOR, CONF_FAN
+from .const import DOMAIN
+
+@callback
+def configured_instances(hass):
+    """Return a set of configured instances."""
+    return set(entry.title for entry in hass.config_entries.async_entries(DOMAIN))
 
 class BathroomHumidityFanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Bathroom Humidity Fan."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        return BathroomHumidityFanOptionsFlowHandler(config_entry)
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title="Bathroom Humidity Fan", data=user_input)
+            if user_input["name"] in configured_instances(self.hass):
+                errors["name"] = "name_exists"
+            else:
+                return self.async_create_entry(title=user_input["name"], data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required(CONF_SENSOR): EntitySelector(domain="sensor"),
-                vol.Required(CONF_FAN): EntitySelector(domain="switch")
-            })
-        )
-
-class BathroomHumidityFanOptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle options flow."""
-
-    def __init__(self, config_entry):
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        """Manage the options."""
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema({
-                vol.Required(CONF_SENSOR, default=self.config_entry.data[CONF_SENSOR]): EntitySelector(domain="sensor"),
-                vol.Required(CONF_FAN, default=self.config_entry.data[CONF_FAN]): EntitySelector(domain="switch")
-            })
+                vol.Required("name"): str,
+                vol.Required("sensor_entity_id"): str,
+                vol.Required("fan_entity_id"): str,
+            }),
+            errors=errors,
         )
