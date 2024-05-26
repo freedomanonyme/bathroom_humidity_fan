11afbfd36688bd394ea010fd309566bee37b831c
freedomanonyme <58370978+freedomanonyme@users.noreply.github.com>
2024-05-26 11:49:35 -0400

    Update __init__.py

--- custom_components/bathroom_humidity_fan/__init__.py
+++ custom_components/bathroom_humidity_fan/__init__.py
@@ -1,22 +1,19 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
 
-async def async_setup(hass, config):
+async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Bathroom Humidity Fan component."""
-    hass.data[DOMAIN] = {}
     return True
 
-async def async_setup_entry(hass, config_entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Bathroom Humidity Fan from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
-    )
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "switch")
-    )
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
     return True
 
-async def async_unload_entry(hass, config_entry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
-    await hass.config_entries.async_forward_entry_unload(config_entry, "switch")
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
